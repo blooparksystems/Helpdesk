032c7c00ce4b32a9ced2462b064c04e5fefc6f0a
Jeferson Moura <jefmmoreira@gmail.com>
2016-01-30 20:49:47 -0200

    Created models and some methods #6

---
+++ models/models.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields
+
+class Order(models.Model)
+    _name = 'helpdesk.order'
+
+    partner_id = fields.Many2one(
+        'res.partner', string='Employee_Manager',
+        default=lambda self: self.env.user.company_id.partner_id)
+    name = field.Char(string="Tile", required=True)
+    content = field.Text(string="Description", required=True)
+    solved = field.Boolean(default=False)
+    deleted = field.Boolean(default=False)
+
+    @api.multi
+    def write(self, vals):
+        res = super(Order, self).write(vals)
+        if vals.get('partner_id'):
+            self.message_subscribe([vals['partner_id']])
+        return res
+
+    @api.model
+    def create(self, vals):
+        registration = super(Order, self).create(vals)
+        registration.sudo().confirm_registration()
+        return registration
+
+
+class Answer(models.Model)
+    _name = 'helpdesk.answer'
+
+    order_id = fields.Many2one(
+        'helpdesk.order', string='Order', required=True,
+        readonly=True)
+    user_id = fields.Many2one(
+        'res.users', string='Agent_Manager',
+        default=lambda self: self.env.user)
+    content = field.Char(string="Message", required=True)
+
+    @api.multi
+    def write(self, vals):
+        res = super(Answer, self).write(vals)
+        if vals.get('user_id'):
+            self.message_subscribe([vals['user_id']])
+        return res
+
+    @api.model
+    def create(self, vals):
+        registration = super(Order, self).create(vals)
+        registration.sudo().confirm_registration()
+        return registration
+
+
+class Track_Follow(models.Model)
+    _name = 'helpdesk.track_follow'
+
+    order_id = fields.Many2one(
+        'helpdesk.order', string='Order', required=True,
+        readonly=True)
+    user_id = fields.Many2one(
+        'res.users', string='Agent_Manager',
+        default=lambda self: self.env.user)
+    partner_id = fields.Many2one(
+        'res.partner', string='Employee_Manager',
+        default=lambda self: self.env.user.company_id.partner_id)
