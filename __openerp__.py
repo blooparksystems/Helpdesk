fac5515340dbbf52b813c44b019793dd4cb292a4
Jeferson Moura <jefmmoreira@gmail.com>
2016-01-30 18:58:07 -0200

    Initializing the project

---
+++ __openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "helpdesk",
+
+    'summary': "",
+
+    'description': "",
+
+    'author': "Jeferson Moura and Taina Lima",
+    'website': "http://www.ourcompany.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
